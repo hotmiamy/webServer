58b2865d72c6ceb176338e1989963186b66f003e
hmigl <88746406+hmigl@users.noreply.github.com>
2024-03-24 16:10:17 -0300

    feat: implement Cgi (for GET method)

--- srcs/Cgi.cpp
+++ srcs/Cgi.cpp
@@ -1,59 +1,88 @@
 #include "Cgi.hpp"
 
-namespace Cgi {
+Cgi::Cgi(const Cgi &rhs) { *this = rhs; }
 
-bool isDefined(ReqParsing &r) {
-    if (!r.getLocation()) {
-        return false;
+Cgi::Cgi(ReqParsing &request)
+    : _request(request), _script(), _binaryAbsPath(), _pipedes(), _out() {}
+
+Cgi &Cgi::operator=(const Cgi &other) {
+    if (this != &other) {
     }
-    return r.getLocation()->cgi;
+    return *this;
 }
 
-void execute(ReqParsing &r) {
-    std::cout << "executando o cgi...\n";
+Cgi::~Cgi() {}
 
-    std::string script = "server_root" + r.getUrl();
-    std::cout << "script: " << script << '\n';
-    std::string binaryAbsPath = ServerUtils::getAbsPath("python");
-    std::cout << "binaryAbsPath:" << binaryAbsPath << '\n';
+void Cgi::_setup() {
+    _script = "./server_root" + _request.getUrl();
+    _binaryAbsPath = ServerUtils::getAbsPath("python");
+}
 
-    int pipedes[2];
-    if (pipe(pipedes) < 0) {
-        std::perror("cgi pipe");
-        return;
+void Cgi::execute() {
+    _setup();
+
+    if (pipe(_pipedes) < 0) {
+        std::perror("pipe (cgi)");
     }
+    dup2(_pipedes[0], STDIN_FILENO);
+    dup2(_pipedes[1], STDOUT_FILENO);
 
     pid_t pid = fork();
     if (-1 == pid) {
-        perror("cgi fork");
-        return;
+        return std::perror("fork (cgi)");
     }
-
     if (0 == pid) {
-        childRoutine(r);
-    } else {
+        _childRoutine();
+    }
+    _parentRoutine(pid);
+}
+
+void Cgi::_childRoutine() {
+    const char *argv[] = {_binaryAbsPath.c_str(), _script.c_str(), NULL};
+    std::vector<std::string> envpVec = _setupEnv();
+    const char *envp[] = {envpVec[0].c_str(), envpVec[1].c_str(),
+                          envpVec[2].c_str(), envpVec[3].c_str(),
+                          envpVec[4].c_str(), envpVec[5].c_str(),
+                          envpVec[6].c_str(), envpVec[7].c_str(),
+                          envpVec[8].c_str(), NULL};
+
+    if (execve(argv[0], (char *const *)argv, (char *const *)envp) < 0) {
+        std::perror("execve (cgi)");
     }
 }
 
-void childRoutine(ReqParsing &r) {
-    std::vector<std::string> envpVec = setupEnv(r);
+void Cgi::_parentRoutine(ssize_t pid) {
+    waitpid(pid, NULL, WNOHANG);
+    const size_t nbyte = 4096;
+    char buf[nbyte + 1];
+    // bzero(buf, nbyte);
+
+    ssize_t ret = read(_pipedes[0], buf, nbyte);
+    if (-1 == ret) {
+        perror("read (cgi)");
+    } else {
+        _out += buf;
+    }
+
+    close(_pipedes[0]);
+    close(_pipedes[1]);
 }
 
-std::vector<std::string> setupEnv(ReqParsing &r) {
+std::vector<std::string> Cgi::_setupEnv() {
     std::vector<std::string> envpVec;
     envpVec.push_back("SERVER_PROTOCOL=HTTP/1.1");
     envpVec.push_back("GATEWAY_INTERFACE=CGI/1.1");
     envpVec.push_back("SERVER_SOFTWARE=webserv");
-    // todo: change these
+    // TODO: improve these
     envpVec.push_back("REQUEST_METHOD=GET");
     envpVec.push_back("SERVER_NAME=localhost");
     envpVec.push_back("SERVER_PORT=8080");
     envpVec.push_back("CONTENT_TYPE=text/html");
-    std::stringstream len;
-    len << r.getBody().length();
-    envpVec.push_back("CONTENT_LENGTH=" + len.str());
-    envpVec.push_back("SCRIPT_NAME=");
+    std::stringstream ss;
+    ss << _request.getBody().length();
+    envpVec.push_back("CONTENT_LENGTH=" + ss.str());
+    envpVec.push_back("SCRIPT_NAME=" + _script);
     return envpVec;
 }
 
-}  // namespace Cgi
+const std::string &Cgi::getOut() const { return _out; }
